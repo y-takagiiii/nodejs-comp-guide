12a80e32f77cfc7d4f3125625ae34b0861556ace
yuritakagi <takagiyuri0712@gmail.com>
2023-06-27 07:59:44 +0900

    add: ??ールバック関数について学ぼう

--- 01_js_basic/040_callback/start/main.js
+++ 01_js_basic/040_callback/start/main.js
@@ -1,15 +1,13 @@
+// コールバック関数 引数に渡された関数のこと
+function print(callback) {
+  // const result = callback();
+  const result = callback(2); // 引数を渡してあげる
+  console.log(result);
+}
+
 function fn(number) {
     return number * 2;
   }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+
+// print(fn); // => 引数がない(undefined * 2 === NaN)
+print(fn); // => 4
