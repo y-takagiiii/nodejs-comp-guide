4d4651ff49f09f84f73cdf68728f5326acd7cb5a
yuritakagi <takagiyuri0712@gmail.com>
2023-06-27 18:56:25 +0900

    add: CommonJsのモジュールの取り扱い方法について学ぼう

---
+++ 02_node_basic/040_module_system/start/calc.js
@@ -0,0 +1,13 @@
+function plus(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  // plus: plus,の省略形
+  plus,
+  minus,
+};
--- 02_node_basic/040_module_system/start/example.js
+++ 02_node_basic/040_module_system/start/example.js
@@ -1,5 +1,9 @@
-function plus(a, b) {
-  return a + b;
-}
+// exportされたモジュールオブジェクトを受け取る
+// const calc = require('./calc');
+
+// 分割代入でplusだけ受け取る
+const { plus } = require('./calc');
 const result = plus(1, 2);
+
+// const result = calc.plus(1, 2);
 console.log(result);
