757d137f2c9f35e63c47194b9582171c861ed5f9
yuritakagi <takagiyuri0712@gmail.com>
2023-06-27 13:51:18 +0900

    add: truthyとfalthyについて学ぼう

--- 01_js_basic/080_truthy_falsy/start/main.js
+++ 01_js_basic/080_truthy_falsy/start/main.js
@@ -13,27 +13,31 @@ NaN (Not a Number)
 
 const a = 0;
 let result = a ? 10 : -10;
-console.log(result);
+// console.log(result);
 
 const falsy = 0;
 const truthy = 1;
-console.log(Boolean(truthy));
-console.log(Boolean(falsy));
+// console.log(Boolean(truthy));
+// console.log(Boolean(falsy));
 
 // 論理積 (&&) について
+// falthyな値があればそれを変数に格納、なければ最後の値を格納
 const resultA = "" && "foo";
+const resultG = 2 && 0;
 const resultB = 2 && 1 && 0 && 3;
 const resultC = "foo" && 4;
 
-// console.log(resultA);
-// console.log(resultB);
-// console.log(resultC);
+console.log(resultA); // => 空文字
+console.log(resultG); // => 0
+console.log(resultB); // => 0
+console.log(resultC); // => 4
 
 // 理論和 (||) について
+// truethyな値があればそれを変数に格納、なければ最後の値を格納
 const resultD = "" || "foo";
 const resultE = 0 || 2 || 0;
 const resultF = "foo" || 4;
 
-console.log(resultD);
-console.log(resultE);
-console.log(resultF);
+console.log(resultD); // => foo
+console.log(resultE); // => 2
+console.log(resultF); // => foo
