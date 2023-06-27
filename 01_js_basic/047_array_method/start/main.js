f833aa1cb9833f1b9e32ae9abdc4000fdf501a3c
yuritakagi <takagiyuri0712@gmail.com>
2023-06-27 09:39:41 +0900

    add: Arrayオブジェクトのメソッドの使い方

--- 01_js_basic/047_array_method/start/main.js
+++ 01_js_basic/047_array_method/start/main.js
@@ -1,2 +1,34 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+for(let i = 0; i < arry.length; i++) {
+  const val = arry[i] * 2;
+  if(val > 50) {
+    newArry.push(arry[i] * 2);
+  }
+};
+
+console.log(newArry);
+
+// const newArry2 = arry.map(val => val * 2);
+
+// アロー関数の処理を複数行記述する
+// const newArry2 = arry.map(val => {
+//   console.log(val);
+//   return val * 3;
+// });
+
+// アロー関数の中に複数の引数を指定する
+// const newArry2 = arry.map((val, i, arry) => {
+//   console.log(i, val, arry);
+//   return val * 3;
+// });
+
+const newArry2 = arry.map((val, i, arry) => {
+  console.log(i, val, arry);
+  return val * 3;
+});
+console.log(newArry2);
+
+const newArry3 = newArry2.filter(val => val > 50);
+console.log(newArry3);
