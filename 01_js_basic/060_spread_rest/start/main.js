b1e8a52c3d701d4e6674bf103d901b9358007d18
yuritakagi <takagiyuri0712@gmail.com>
2023-06-27 12:25:08 +0900

    add: スプレッド演算子について学ぼう

--- 01_js_basic/060_spread_rest/start/main.js
+++ 01_js_basic/060_spread_rest/start/main.js
@@ -1,13 +1,19 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
-console.log(result);
+// const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+
+// スプレッド演算子
+const result = Math.max(...nums);
+console.log(result); // => 10
 
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
+// スプレッド演算子でarr1の配列が展開されてnewArrが定義される
+let newArr = [...arr1]; // => (3) [1, 2, 3]
+let newArr2 = [...arr1, 10, ...arr2]; // => (7) [1, 2, 3, 10, 4, 5, 6]
 
+console.log(newArr);
 console.log(newArr2);
 
 const obj = {
@@ -16,10 +22,15 @@ const obj = {
 };
 const newObj = { ...obj };
 
-console.log(newObj);
+console.log(newObj); // => { name: 'Tom', age: 22 }
+newObj.name = 'John';
+// スプレッド演算子を使って新たに作成したオブジェクトのプロパティを変更しても、元のオブジェクトには影響しない
+console.log(newObj, obj); // => {name: 'John', age: 22} {name: 'Tom', age: 22}
 
+// レストパラメータ、残余引数
 const restA = (...argA) => console.log(argA);
-restA(1, 3, 4)
+// 引数1, 3, 4が配列になって出力される
+restA(1, 3, 4) // => (3) [1, 3, 4]
 
 const restB = (n, ...argB) => console.log(argB);
-restB(1, 3, 4)
+restB(1, 3, 4) // => [3, 4]
