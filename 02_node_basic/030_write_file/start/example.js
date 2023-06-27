42f039b0cf9ea29d539eb57ec80e495fb07b60e5
yuritakagi <takagiyuri0712@gmail.com>
2023-06-27 15:31:20 +0900

    add: ファイルに文字を書き込んでみよう

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,16 @@
-console.log('hello, node.js');
+// ファイルへの書き込み
+// __dirname nodeコマンドを実行したフォルダまでの絶対パス
+// __filename nodeコマンドを実行したファイルまでの絶対パス
+
+// fsモジュールの読み込み
+const fs = require('fs');
+
+// console.log(__filename);
+
+// writeFileSync(ファイル名, 書き込むテキスト)
+// 相対パスを指定するとnodeを実行したディレクトリからの相対パスになるので注意
+// fs.writeFileSync('./test.txt', 'hello, node.js');
+
+// __dirnameを使って書き換え
+fs.writeFileSync(__dirname + '/test.txt', 'hello, node.js');
+// console.log('hello, node.js');
