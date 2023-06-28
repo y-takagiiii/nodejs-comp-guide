4669954e6cf4722df61fbdbf56d146ceafb4c16c
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 17:12:00 +0900

    add: HTTPメソッドについて学ぼう2

--- 06_backend/060_http_method/start/app.mjs
+++ 06_backend/060_http_method/start/app.mjs
@@ -16,16 +16,23 @@ const server = http.createServer(function (req, res) {
   res.writeHead(200, { 'content-type': 'text/html; charset=UTF-8' });
   console.log(req.url);
   if (req.url === '/') {
-    res.write(`<a href="/result?param1=パラメータ1&params2=パラメータ2">Get Method Link</a>`);
+    res.write(`<a href="/result?param1=1&params2=2">Get Method Link</a>`);
     res.end(`
       <form action="/result" method="POST">
         <input type="text" name="title">
         <input type="submit">
       </form>
     `);
-  } else if (req.url === '/bye') {
-    res.end('bye');
   } else {
+    console.log(req.url);
+    console.log(req.method);
+    if(req.method === "GET") {
+      // GETのパラメータを取得
+      const queryString = req.url.split("?")[1];
+      const params = new URLSearchParams(queryString);
+      console.log(params);
+      // console.log(params.has('param1'));
+    }
     res.end(req.url);
   }
 });
