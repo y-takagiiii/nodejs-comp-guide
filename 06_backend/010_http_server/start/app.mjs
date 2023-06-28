177532871c1e6674081ef5a40ff3cc1fb562d16a
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 15:29:04 +0900

    add: Content-Typeについて学ぼう

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -1,11 +1,26 @@
 import * as http from "http";
 
+// content-type:
+// ブラウザとサーバーが通信しているコンテンツのフォーマットと文字コード情報
+// リクエストヘッダーに設定するかmetaタグで設定する
 const server = http.createServer(function(req, res) {
   console.log(req.url);
+  // res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
   if (req.url ==='/hello') {
-    // res.endの処理はブラウザで行われるのでwindowオブジェクトが使える
-    // それ以外のコードはnode.js場で行われるの(windowオブジェクトは使えない)
-    res.end('<script>window.alert("frontend")</script>');
+    // res.end('<h1>こんにちは</h1>');
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>こんにちは</h1>
+      </body>
+      </html>
+    `);
   } else if (req.url === '/bye') {
     res.end('bye');
   }
