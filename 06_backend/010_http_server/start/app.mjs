d083fce0a389d79f305af4a274b0b1c77f3ae0a3
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 15:08:38 +0900

    add: JavaScriptが実行される場所を理解しよう

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -3,7 +3,9 @@ import * as http from "http";
 const server = http.createServer(function(req, res) {
   console.log(req.url);
   if (req.url ==='/hello') {
-    res.end('hello');
+    // res.endの処理はブラウザで行われるのでwindowオブジェクトが使える
+    // それ以外のコードはnode.js場で行われるの(windowオブジェクトは使えない)
+    res.end('<script>window.alert("frontend")</script>');
   } else if (req.url === '/bye') {
     res.end('bye');
   }
