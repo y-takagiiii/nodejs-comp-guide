1fc02f4e9e4bf0d3295c2003628ad0c4e98959eb
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 12:46:49 +0900

    add: パッケージが読み込まれる流れを学ぼう

--- 03_npm/010_intro/start/main.mjs
+++ 03_npm/010_intro/start/main.mjs
@@ -1,12 +1,20 @@
 // lib/lodash.jsを取り込む
 // import _ from "../lib/lodash.js";
 
-// npmでlodashをインストールし、node_modulesから読み込む
+// npm install lodashを実行後
+// パスの指定がない場合にはnode_moduleから一致するフォルダ名を探して読み込む
 import _ from "lodash";
+
+// cloneDeepだけを読み込む場合
+// import cloneDeep from "lodash/cloneDeep.js";
+// CJSで動かすと.jsを省略できる
+// const cloneDeep = require("lodash/cloneDeep");
 
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
 const cloned = _.cloneDeep(original);
+// cloneDeepだけを読み込んだ場合
+// const cloned = cloneDeep(original);
 // 複製したオブジェクトのプロパティを変更
 cloned.prop.nested = "new value";
 console.log(original, cloned);
