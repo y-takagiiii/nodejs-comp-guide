8d75cc12ec36880beb6d578c046f3bfff3057bb1
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 16:12:05 +0900

    add: HTTPステータスコードについて学ぼう

--- 06_backend/050_http_status/start/app.mjs
+++ 06_backend/050_http_status/start/app.mjs
@@ -2,11 +2,14 @@ import * as http from "http";
 
 const server = http.createServer(function(req, res) {
     console.log(req.url);
-    res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
+    // res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
     if(req.url === '/hello') {
+        res.writeHead(302, {location: '/redirected'});
         res.end(`<h1>こんにちは</h1>`)
     } else if(req.url === '/bye') {
         res.end('bye');
+    } else {
+        res.end(req.url);
     }
 });
 
