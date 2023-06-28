4a5cc0d79257a3ec546e9973b0ab6f1cd9214199
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 12:32:31 +0900

    add: なぜnpmが必要なの？

--- 03_npm/010_intro/start/main.mjs
+++ 03_npm/010_intro/start/main.mjs
@@ -1,2 +1,13 @@
+// lib/lodash.jsを取り込む
+// import _ from "../lib/lodash.js";
+
+// npmでlodashをインストールし、node_modulesから読み込む
+import _ from "lodash";
+
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
+const cloned = _.cloneDeep(original);
+// 複製したオブジェクトのプロパティを変更
+cloned.prop.nested = "new value";
+console.log(original, cloned);
+
