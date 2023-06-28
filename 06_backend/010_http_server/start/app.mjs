286120cdb5737a0824050e6b3a20c714cf0983f2
yuritakagi <takagiyuri0712@gmail.com>
2023-06-28 14:49:02 +0900

    add: パスごとに制御を加えてみよう

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -2,7 +2,11 @@ import * as http from "http";
 
 const server = http.createServer(function(req, res) {
   console.log(req.url);
-  res.end('hello');
+  if (req.url ==='/hello') {
+    res.end('hello');
+  } else if (req.url === '/bye') {
+    res.end('bye');
+  }
 });
 
 server.listen(8000);
